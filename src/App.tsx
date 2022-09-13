59508ed72de28fedad7db6308b1cb24bb0aebfd9
aftab-alam-khan <aftab.khan@pager.com>
2022-09-13 17:46:01 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import socketService from './services/socketService';
+import { JoinRoom } from './components/joinRoom';
+import GameContext, { IGameContextProps } from './gameContext';
+import { Game } from './components/game';
+
+type symbolType = "x"| "o"
+
+const AppContainer = styled.div`
+width: 100%;
+height: 100%;
+display: flex;
+flex-direction: column;
+align-items: center;
+padding: 1em;
+`;
+
+const WelcomeText = styled.h1`
+  margin: 0;
+  color: #8e44ad;
+`;
+
+const MainContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function App() {
+
+  const [isInRoom, setInRoom] = useState(false)
+  const [playerSymbol, setPlayerSymbol] = useState<symbolType>('x');
+  const [isPlayerTurn, setPlayerTurn] = useState(false);
+  const [isGameStarted, setGameStarted] = useState(false);
+
+  const connectSocket = async () => {
+    const socket = await socketService.connect('http://localhost:9000').catch((err) => {
+      console.log('Error: ', err);
+      
+    })
+    console.log(' const socket', socket);
+    
+  }
+
+  useEffect(() => {
+    connectSocket();
+  }, [])
+
+  const gameContextValue: IGameContextProps = {
+    isInRoom,
+    setInRoom,
+    playerSymbol,
+    setPlayerSymbol,
+    isPlayerTurn,
+    setPlayerTurn,
+    isGameStarted,
+    setGameStarted,
+  }
+
+  return (
+    <GameContext.Provider value={gameContextValue}>
+    <AppContainer>
+      <WelcomeText> Welcome to Tic-Tac-Toe</WelcomeText>
+      <MainContainer>
+        {!isInRoom && <JoinRoom />}
+        {isInRoom && <Game />}
+      </MainContainer>
+    </AppContainer>
+    </GameContext.Provider>
+  );
+}
+
+export default App;
